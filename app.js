38c367ad4ac819341c69b333b57d038952fb5310
Buhori <buhorics@gmail.com>
2023-11-07 12:56:06 +0900

    perubahan penulisan rumus perhitungan persen yg ditampung oleh variabel

--- app.js
+++ app.js
@@ -13,37 +13,45 @@ rl.question("Masukkan golongan : ", (golongan) => {
     let GolonganB = 6500000;
     let GolonganC = 9500000;
 
+    //rumus perhitungan persen
+    // eval adalah fungsi yang digunakan untuk menghitung persen
+    const satuJam = eval(30 / 100 );
+    const duaJam = eval(32 / 100);
+    const tigaJam = eval(34 / 100) ;
+    const empatJam = eval(36 / 100);
+    const limaJam = eval(38 / 100);
+
     //masukan kondisi ketika user menekan golongan dan jam lembur
     if (golongan == 'A' && lembur == 1) {
-        console.log('Jumlah penghasilannya :', GolonganA + eval(30 / 100) * GolonganA); // eval adalah fungsi yang digunakan untuk menghitung persen
+        console.log('Jumlah penghasilannya :', GolonganA + satuJam * GolonganA); 
     }else if (golongan == 'A' && lembur == 2) {
-        console.log('Jumlah penghasilannya :', GolonganA + eval(32 / 100) * GolonganA);
+        console.log('Jumlah penghasilannya :', GolonganA + duaJam * GolonganA);
     }else if(golongan == 'A' && lembur == 3) {
-        console.log('Jumlah penghasilannya :', GolonganA + eval(34 / 100) * GolonganA);
+        console.log('Jumlah penghasilannya :', GolonganA + tigaJam * GolonganA);
     }else if(golongan == 'A' && lembur == 4){
-        console.log('Jumlah penghasilannya :', GolonganA + eval(36 / 100) * GolonganA);
+        console.log('Jumlah penghasilannya :', GolonganA + empatJam * GolonganA);
     }else if(golongan == 'A' && lembur >= 5) {
-        console.log('Jumlah penghasilannya :', GolonganA + eval(38 / 100) * GolonganA);
+        console.log('Jumlah penghasilannya :', GolonganA + limaJam * GolonganA);
     }else if (golongan == 'B' && lembur == 1) {
-        console.log('Jumlah penghasilannya :', GolonganB + eval(30 / 100) * GolonganB);
+        console.log('Jumlah penghasilannya :', GolonganB + satuJam * GolonganB);
     }else if (golongan == 'B' && lembur == 2) {
-        console.log('Jumlah penghasilannya :', GolonganB + eval(32 / 100) * GolonganB);
+        console.log('Jumlah penghasilannya :', GolonganB + duaJam * GolonganB);
     }else if(golongan == 'B' && lembur == 3) {
-        console.log('Jumlah penghasilannya :', GolonganB + eval(34 / 100) * GolonganB);
+        console.log('Jumlah penghasilannya :', GolonganB + tigaJam * GolonganB);
     }else if(golongan == 'B' && lembur == 4){
-        console.log('Jumlah penghasilannya :', GolonganB + eval(36 / 100) * GolonganB);
+        console.log('Jumlah penghasilannya :', GolonganB + empatJam * GolonganB);
     }else if(golongan == 'B' && lembur >= 5) {
-        console.log('Jumlah penghasilannya :', GolonganB + eval(38 / 100) * GolonganB);
+        console.log('Jumlah penghasilannya :', GolonganB + limaJam * GolonganB);
     }else if (golongan == 'C' && lembur == 1) {
-        console.log('Jumlah penghasilannya :', GolonganC + eval(30 / 100) * GolonganC);
+        console.log('Jumlah penghasilannya :', GolonganC + satuJam * GolonganC);
     }else if (golongan == 'C' && lembur == 2) {
-        console.log('Jumlah penghasilannya :', GolonganC + eval(32 / 100) * GolonganC);
+        console.log('Jumlah penghasilannya :', GolonganC + duaJam * GolonganC);
     }else if(golongan == 'C' && lembur == 3) {
-        console.log('Jumlah penghasilannya :', GolonganC + eval(34 / 100) * GolonganC);
+        console.log('Jumlah penghasilannya :', GolonganC + tigaJam * GolonganC);
     }else if(golongan == 'C' && lembur == 4){
-        console.log('Jumlah penghasilannya :', GolonganC + eval(36 / 100) * GolonganC);
+        console.log('Jumlah penghasilannya :', GolonganC + empatJam * GolonganC);
     }else if(golongan == 'C' && lembur >= 5) {
-        console.log('Jumlah penghasilannya :', GolonganC + eval(38 / 100) * GolonganC);
+        console.log('Jumlah penghasilannya :', GolonganC + limaJam * GolonganC);
     }else {
           console.log("golongan tidak ada");
         }
